e19686048b635169c6d07121107a15443a56b343
lululu <1709196413@qq.com>
2025-06-03 21:01:25 +0800

    修改医生管理、患者管理的Mapper层,主要是改用调用存储过程的方式实现业务逻辑。 基本完成科室管理的增删改查。 医生表与科室表建立联系,医生表dept_id作为外链。

---
+++ src/main/java/com/Entity/Pojo/ProcedureResult.java
@@ -0,0 +1,13 @@
+package com.Entity.Pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProcedureResult {
+
+    private String message;
+}
